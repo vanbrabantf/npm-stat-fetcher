f7933cbe551dbcfbe16b9b39a0662f15f60c894e
Frederick Vanbrabant <frederick.vanbrabant@gmail.com>
2017-10-06 14:00:07 +0200

    initial commit

---
+++ tests/unit/initialTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class initialTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     */
+    public function testSuitWorksTest()
+    {
+        $this->assertSame(true, true);
+    }
+}
